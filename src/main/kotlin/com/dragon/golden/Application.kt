c2bc2ea4a0464da1ce5ea2b6dffe98f1fa6bedb4
evgeniymoiseev <evgeniy.moiseev.1985@gmail.com>
2024-04-08 08:44:15 +0300

    heroku

--- src/main/kotlin/com/dragon/golden/Application.kt
+++ src/main/kotlin/com/dragon/golden/Application.kt
@@ -7,8 +7,8 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "192.168.0.103", module = Application::module).start(wait = true)
-    //embeddedServer(Netty, port = System.getenv("PORT")?.toInt() ?: 8080, module = Application::module).start(wait = true)
+    //embeddedServer(Netty, port = 8080, host = "192.168.0.103", module = Application::module).start(wait = true)
+    embeddedServer(Netty, port = System.getenv("PORT")?.toInt() ?: 8080, module = Application::module).start(wait = true)
 }
 
 fun Application.module() {
